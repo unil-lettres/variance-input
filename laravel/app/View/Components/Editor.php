298e68523ce63674f6d401cb891e5bd562037df1
David M. <davidmiserez@gmail.com>
2025-10-23 10:50:56 +0200

    Move editor in its own blade component

---
+++ laravel/app/View/Components/Editor.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\View\Components;
+
+use App\Models\Comparison;
+use App\Models\Version;
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class Editor extends Component
+{
+
+    /**
+     * Create a new component instance.
+     */
+    public function __construct(
+        public Version $version,
+        public Comparison $comparison,
+        public string $xmlContent,
+        public bool $isSource,
+        public bool $isPublished,
+        public bool $canEdit,
+        public array $imagesData,
+    )
+    { }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.editor');
+    }
+}
