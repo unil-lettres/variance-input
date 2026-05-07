1351a0d4dc9d4d1df387208684bbebaae5cc46bb
Julien Ganivet <jganivet1006@gmail.com>
2026-05-07 17:45:51 +0200

    Isolate test storage from local data

--- laravel/tests/TestCase.php
+++ laravel/tests/TestCase.php
@@ -8,11 +8,22 @@ use App\Models\User;
 use App\Models\Work;
 use App\Models\Version;
 use App\Models\Comparison;
+use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 use Illuminate\Support\Facades\File;
 
 abstract class TestCase extends BaseTestCase
 {
+    public function createApplication(): Application
+    {
+        $app = require __DIR__.'/../bootstrap/app.php';
+        $app->useStoragePath(__DIR__.'/../storage/framework/testing');
+        $app->make(Kernel::class)->bootstrap();
+
+        return $app;
+    }
+
     protected function setUp(): void
     {
         parent::setUp();
